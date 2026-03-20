87201f94c5fddaa77bea6a7849ebef7d9998b6f3
floxygit <floxymusic1@gmail.com>
2026-03-20 16:46:00 +0100

    Create Main.php for WebPanel plugin structure

---
+++ src/Main.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace floxygit\WebPanel;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+
+    protected function onEnable(): void{
+    }
+
+    protected function onDisable(): void{
+    }
+}
